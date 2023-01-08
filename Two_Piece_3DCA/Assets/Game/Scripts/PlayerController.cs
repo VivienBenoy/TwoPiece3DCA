9f242f1bad68d446845b5c5a64cae5c8c9ca6bcb
VivienBenoy <D00225727@student.dkit.ie>
2023-01-08 22:11:07 +0000

    CameraMovement
    Switched from Virtual to free look camera to accommodate mouse movement

--- Two_Piece_3DCA/Assets/Game/Scripts/PlayerController.cs
+++ Two_Piece_3DCA/Assets/Game/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
 
     private CharacterController controller;
+    public Transform cam;
     private Vector3 moveDirection = Vector3.zero;
 
     void Start()
@@ -18,11 +21,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         if (controller.isGrounded)
         {
             // We are grounded, so recalculate movedirection directly from axes
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
+            moveDirection = new Vector3(horizontal, 0, vertical);
+            if(moveDirection.magnitude>=0.1f)
+            {
+                float targetangle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
 
             if (Input.GetButton("Jump"))
             {
