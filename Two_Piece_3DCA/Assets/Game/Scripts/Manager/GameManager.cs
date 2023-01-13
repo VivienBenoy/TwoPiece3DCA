c0a1f95166845245598f78ace2b2c8369747d946
VivienBenoy <D00225727@student.dkit.ie>
2023-01-13 15:34:45 +0000

    Gamestate
    Updating the Gamestate using the buttons from the canvas

--- Two_Piece_3DCA/Assets/Game/Scripts/Manager/GameManager.cs
+++ Two_Piece_3DCA/Assets/Game/Scripts/Manager/GameManager.cs
@@ -1,18 +1,84 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+using UnityEngine.UI;
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager: MonoBehaviour
     {
-        
-    }
+        public int gamestate;
+        public bool isPaused;
+        public Button playButton;
+        public Button quitButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Start()
+        {
+            gamestate = 1;
+            InitializeEnvironment();
+            StartCoroutine(GameLoop());
+        }
+
+        private IEnumerator GameLoop()
+        {
+            yield return StartCoroutine(StartLevel());
+            yield return StartCoroutine(PlayLevel());
+            yield return StartCoroutine(EndLevel());
+        }
+
+        private IEnumerator EndLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerator StartLevel()
+        {
+            throw new NotImplementedException();
+        }
+        public void Play()
+        {
+            isPaused = false;
+        }
+
+        public void Pause()
+        {
+            isPaused = true;
+        }
+
+        private IEnumerator PlayLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeEnvironment()
+        {
+            throw new NotImplementedException();
+        }
+        private void OnDestroy()
+        {
+            StopAllCoroutines();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                isPaused = !isPaused;
+            }
+
+            if (isPaused)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+
+            playButton.onClick.AddListener(OnClick1);
+            quitButton.onClick.AddListener(OnClick2);
+        }
+        private void OnClick1()
+        {
+            gamestate = 2;
+        }
+        private void OnClick2()
+        {
+            gamestate = 3;
+        }
     }
 }
